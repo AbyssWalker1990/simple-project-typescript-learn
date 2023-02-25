96f1ce440972655b70fc97ff7a4b759a244de95b
AbyssWalker1990 <bordiian.v@gmail.com>
2023-02-25 13:58:26 +0200

    started working with projectInput class

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 'use strict'
-console.log('Hello')
+class projectInput {
+  constructor () {
+    const templateEl = document.getElementById('project-input')
+    const hostElement = document.getElementById('app')
+    if (templateEl != null) {
+      this.templateElement = document.getElementById('project-input')
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+    if (hostElement !== null) {
+      this.hostElement = document.getElementById('app')
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Hello')
+class projectInput {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+
+  constructor () {
+    const templateEl = document.getElementById('project-input')
+    const hostElement = document.getElementById('app')
+    if (templateEl != null) {
+      this.templateElement = document.getElementById('project-input') as HTMLTemplateElement
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+    if (hostElement !== null) {
+      this.hostElement = document.getElementById('app') as HTMLDivElement
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+  }
+}
