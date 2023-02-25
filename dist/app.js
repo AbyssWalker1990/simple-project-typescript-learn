65cd39d0c1349013d6e5beb3b7e9e8e9a42606fa
Vova <bordiian.v@gmail.com>
2023-02-25 18:21:54 +0200

    created autobind decorator for practice

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,79 @@
-'use strict'
-class ProjectInput {
-  constructor () {
-    const templateEl = document.getElementById('project-input')
-    const hostElement = document.getElementById('app')
-    if (templateEl != null) {
-      this.templateElement = document.getElementById('project-input')
-    } else {
-      throw new Error('There is no element with this identifier')
-    }
-    if (hostElement !== null) {
-      this.hostElement = document.getElementById('app')
-    } else {
-      throw new Error('There is no element with this identifier')
-    }
-    const importNode = document.importNode(this.templateElement.content, true)
-    this.element = importNode.firstElementChild
-    this.attach()
-  }
-
-  attach () {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element)
-  }
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    console.log(originalMethod);
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    console.log(adjDescriptor);
+    return adjDescriptor;
 }
-const prjInput = new ProjectInput()
+class ProjectInput {
+    constructor() {
+        const templateEl = document.getElementById('project-input');
+        const hostElement = document.getElementById('app');
+        if (templateEl != null) {
+            this.templateElement = templateEl;
+        }
+        else {
+            throw new Error('There is no element with this identifier');
+        }
+        if (hostElement !== null) {
+            this.hostElement = hostElement;
+        }
+        else {
+            throw new Error('There is no element with this identifier');
+        }
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild;
+        this.element.id = 'user-input';
+        const titleInputEl = this.element.querySelector('#title');
+        const descriptionInputEl = this.element.querySelector('#description');
+        const peopleInputEl = this.element.querySelector('#people');
+        if (titleInputEl !== null) {
+            this.titleInputElement = titleInputEl;
+        }
+        else {
+            throw new Error('There is no element with this identifier');
+        }
+        if (descriptionInputEl !== null) {
+            this.descriptionInputElement = descriptionInputEl;
+        }
+        else {
+            throw new Error('There is no element with this identifier');
+        }
+        if (peopleInputEl !== null) {
+            this.peopleInputElement = peopleInputEl;
+        }
+        else {
+            throw new Error('There is no element with this identifier');
+        }
+        this.configure();
+        this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+__decorate([
+    Autobind
+], ProjectInput.prototype, "submitHandler", null);
+const prjInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,74 @@
+// Autobind decorator
+function Autobind (_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get () {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+// Project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement
   hostElement: HTMLDivElement
   element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
   constructor () {
     const templateEl = document.getElementById('project-input')
     const hostElement = document.getElementById('app')
     if (templateEl != null) {
-      this.templateElement = document.getElementById('project-input') as HTMLTemplateElement
+      this.templateElement = templateEl as HTMLTemplateElement
     } else {
       throw new Error('There is no element with this identifier')
     }
     if (hostElement !== null) {
-      this.hostElement = document.getElementById('app') as HTMLDivElement
+      this.hostElement = hostElement as HTMLDivElement
     } else {
       throw new Error('There is no element with this identifier')
     }
     const importNode = document.importNode(this.templateElement.content, true)
     this.element = importNode.firstElementChild as HTMLFormElement
+    this.element.id = 'user-input'
+
+    const titleInputEl = this.element.querySelector('#title')
+    const descriptionInputEl = this.element.querySelector('#description')
+    const peopleInputEl = this.element.querySelector('#people')
+    if (titleInputEl !== null) {
+      this.titleInputElement = titleInputEl as HTMLInputElement
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+    if (descriptionInputEl !== null) {
+      this.descriptionInputElement = descriptionInputEl as HTMLInputElement
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+    if (peopleInputEl !== null) {
+      this.peopleInputElement = peopleInputEl as HTMLInputElement
+    } else {
+      throw new Error('There is no element with this identifier')
+    }
+
+    this.configure()
     this.attach()
+  }
+
+  @Autobind
+  private submitHandler (event: Event): void {
+    event.preventDefault()
+    console.log(this.titleInputElement.value)
+  }
+
+  private configure (): void {
+    this.element.addEventListener('submit', this.submitHandler)
   }
 
   private attach (): void {
