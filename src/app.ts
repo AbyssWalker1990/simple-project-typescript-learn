9aae586d26fc2b8aa452e76cc74928a64cc43920
Vova <bordiian.v@gmail.com>
2023-03-01 13:56:50 +0200

    learned to use class-transformer library with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-import _ from 'lodash'
-declare let GLOBAL: any
+import 'reflect-metadata'
+import { plainToClass } from 'class-transformer'
+import { Product } from './product.model'
 
-console.log(_.shuffle([1, 2, 3]))
-console.log(GLOBAL)
+const products = [{ title: 'A carpet', price: 29.99 }, { title: 'A book', price: 10.99 }]
+// const p1 = new Product('A book', 12.99)
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price)
+// })
+
+const loadedProducts = plainToClass(Product, products)
+
+for (const prod of loadedProducts) {
+  console.log(prod)
+}
